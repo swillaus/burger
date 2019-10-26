6dac40bebbe9529ab2a04188772945590308a20e
Your Name <sam@upsidebusinessgroup.com.au>
2019-10-26 15:28:34 -0400

    with remote db

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,20 @@ var mysql = require("mysql");
 
 require('dotenv').config()
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.PASS,
-  database: process.env.DB
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: process.env.PASS,
+    database: process.env.DB
+  });
 
-connection.connect(function(err) {
+}
+
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
